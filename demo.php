143ff8cc25b9736a798b0db7dc6ddff77c189ae4
renxinghui <renxinghui1790@163.com>
2015-12-28 23:12:19 +0800

    edit file

--- demo.php
+++ demo.php
@@ -4,12 +4,12 @@
 	define('KEY','XXXX');
 	define('SECRET', 'XXX');
 	define('HOST','XXXX');
-	
+
 	$file_name  = 'XXXX'; // 文件上传路径
 	$apex = new \apexApi(KEY, SECRET, HOST);
-    $apex->uuid = microtime();
-    $api = 'third.membercard.uploadAvatar';
-    
-    $data['post'] = 'post';
-    $data['img'] = '@'.$file_name;
-    $ret = $apex->api($api,$data,'POST',true);
+	$apex->uuid = microtime();
+	$api = 'third.membercard.uploadAvatar';
+
+	$data['post'] = 'post';
+	$data['img'] = '@'.$file_name;
+	$ret = $apex->api($api,$data,'POST',true);
