e9ba9b18abde2c7528439964b5de790b51cb6caa
renxinghui <renxinghui1790@163.com>
2015-12-28 22:35:51 +0800

    add file

---
+++ demo.php
@@ -0,0 +1,14 @@
+<?php
+	
+	require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'httpClient.class.php');
+	define('KEY','XXXX');
+	define('SECRET', 'XXX');
+	define('HOST','XXXX');
+	
+	$file_name  = 'XXXX'; // 文件上传路径
+	$apex = new \apexApi(KEY, SECRET, HOST);
+    $apex->uuid = microtime();
+    $api = 'third.membercard.uploadAvatar';
+    $data['card_no'] = $card_no;
+    $data['img'] = '@'.$file_name;
+    $ret = $apex->api($api,$data,'POST',true);
